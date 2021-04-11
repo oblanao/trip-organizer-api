7cc2dfff511bd054e638173e62ceee3966e04812
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-11 18:31:40 +0300

    update identity seeds

--- db/resources/identities.js
+++ db/resources/identities.js
@@ -10,6 +10,9 @@ module.exports = async () => {
       password: shhhh,
       active: true,
       confirmed: true,
+      position: 'Breaker of borders',
+      color: '#215f80',
+      photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     },
     {
       email: 'oana@posedaru.ro',
@@ -18,6 +21,9 @@ module.exports = async () => {
       password: shhhh,
       active: false,
       confirmed: true,
+      position: 'Taker of cares',
+      color: '#be4655',
+      photo: 'https://t3.ftcdn.net/jpg/03/67/46/48/360_F_367464887_f0w1JrL8PddfuH3P2jSPlIGjKU2BI0rn.jpg',
     },
   ];
 };
