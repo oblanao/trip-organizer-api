356e07ebe57699bdb61eb6a37b760f670d9c2e30
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-11 18:24:40 +0300

    Adds clean utility to reset db

---
+++ db/clean.js
@@ -0,0 +1,24 @@
+const connectToMongo = require('../functions/connect');
+
+const { Identity, Todo, Country, Trip } = require("../models");
+
+const clean = async () => {
+  await connectToMongo();
+
+  await Identity.deleteMany({})
+  await Todo.deleteMany({})
+  await Country.deleteMany({})
+  await Trip.deleteMany({})
+};
+
+(async () => {
+  try {
+    await clean(); // invoke function
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
+
+module.exports.clean = clean;
