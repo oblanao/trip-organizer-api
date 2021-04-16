89e7b842b6ff411da32eec4bf0c46afbd0deae39
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-16 20:53:55 +0300

    Adds car model

---
+++ models/car.js
@@ -0,0 +1,37 @@
+const { Schema, Types, model } = require('mongoose');
+const { validate } = require('./plugins');
+const { timestamps } = require('./schemas');
+
+const schema = new Schema({
+  identity: {
+    type: Types.ObjectId,
+    ref: 'identity',
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  },
+  features: {
+    type: Array
+  },
+  featured_photo: {
+    type: String,
+  },
+  ...timestamps,
+});
+
+schema.plugin(validate);
+
+const name = 'car';
+module.exports = model(name, schema);
