de694fb601e4fccf90f59a30322ba4059ad230b1
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-16 22:26:55 +0300

    Adds country readMany controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 /* TODO: Add controllers for all models */
 
+const Country = require('./country');
 const Identity = require('./identity');
 const Todo = require('./todo');
 const Trip = require('./trip');
 
 module.exports = {
+  Country,
   Identity,
   Todo,
   Trip,
