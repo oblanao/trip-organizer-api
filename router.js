7bd1c61d88d035e344bf48f99cd1cc4ab55b0a01
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2021-04-17 12:55:50 +0300

    Adds new actors routes to indexRouter

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { errorHandler, fail, notFound, slow } = require('./middleware');
-const { identity, todo, trip } = require('./routes');
+const { accommodation, attraction, car, country, identity, plane, planeSegment, todo, transfer, trip } = require('./routes');
 
 const router = Router();
 module.exports = router;
@@ -10,8 +10,15 @@ router.use(fail);
 router.use(slow);
 
 // use the router instances defined
+router.use(accommodation);
+router.use(attraction);
+router.use(car);
+router.use(country);
 router.use(identity);
+router.use(plane);
+router.use(planeSegment);
 router.use(todo);
+router.use(transfer);
 router.use(trip);
 
 // matches any other HTTP method and route not matched before
